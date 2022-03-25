d24180540fa1564185a76d9e38df1521ce0f027d
Dyon <shedongyong@gmail.com>
2022-03-25 17:27:33 +0800

    add companyShipments query

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,5 +1,10 @@
+import { useContext } from "react";
 import { normalShipments } from "./mock";
+import { companyShipments } from "./mock";
+import { CompanyContext } from "../context";
 
 export const useGetShipmentQuery = () => {
-  return normalShipments;
+  const isCompanyConsole = useContext(CompanyContext);
+
+  return isCompanyConsole ? companyShipments : normalShipments;
 };
